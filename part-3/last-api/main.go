cbab56484b2ade44e0c4a98cd3981a4deeb87720
diedgo05 <233358@ids.upchiapas.edu.mx>
2025-03-26 01:12:49 -0600

    Detalles

--- part-3/last-api/main.go
+++ part-3/last-api/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"log"
 	dependenciesRobot "last-api/robot/infraestructure/dependencies"
 	routesRobot "last-api/robot/infraestructure/routes"
+	"log"
+
 	dependenciesRS "last-api/robot-status/infraestructure/dependencies"
 	routesRS "last-api/robot-status/infraestructure/routes"
+
 	dependenciesH "last-api/historial-de-entregas/infraestructure/dependencies"
 	routesHistorial "last-api/historial-de-entregas/infraestructure/routes"
+
 	dependenciesCircuito "last-api/circuito/infraestructure/dependencies"
 	routesCircuito "last-api/circuito/infraestructure/routes"
+
+	dependenciesDetalles "last-api/detalles-del-pedido/infraestructure/dependencies"
+	routerDetalle "last-api/detalles-del-pedido/infraestructure/routes"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -40,7 +47,9 @@ func main() {
 	dependenciesCircuito.Init()
 	routesCircuito.CircuitRoutes(router)
 
-	//
+	//Detalles del pedido
+	dependenciesDetalles.Init()
+	routerDetalle.Detallesroutes(router)
 
 	port := ":8082"
 	log.Println("Servidor escuchando en el puerto", port)
