bed5a40a3a35411b609b1cf94ca7d33ae7e878c6
jenry eduardo gomez calvo <eduardogomezcalvo851@gmail.com>
2025-03-26 21:14:18 -0600

    Delete part-1 directory

--- part-1/api-main/producto/domain/Producto.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type Producto struct{
-	IdProducto int
-	Nombre string
-	Precio float64
-	Descripcion string
-	Tipo string
-}
--- part-1/api-main/users/domain/Iuser.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type Iuser interface{
-	Save(user *User)error
-	GetUserById(id int)([]User,error)
-	GetAllUser()([]User,error)
-	UpdateUser(id int,user *User)error
-	DeleteUser(id int)error
-}
